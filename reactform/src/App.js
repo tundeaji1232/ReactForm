aad04b5433892a2bd6fe22a35b75a830bc790464
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-05-11 00:29:11 +0100

    styling

--- reactform/src/App.js
+++ reactform/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-      <h2 classNamme="header">SignUp Form</h2>
+      <h2 className="header">SignUp Form</h2>
        <Form className="FormStyl" />
       </div>
     );
